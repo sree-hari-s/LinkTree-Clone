1382ca894a78399d5d5396609c424caf6527b2ee
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-08-23 20:42:55 +0530

    deployement in github pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 import {Avatar} from "primereact/avatar";
 import {Badge} from "primereact/badge";
 
 function App() {
   return (
-    <div>
+    <div className="p-d-flex p-jc-center p-ai-center">
       <Avatar image="/logo512.png" size="xlarge">
         <Badge value="4" severity="danger"/>
       </Avatar>
+      <p>Testing LinkFree</p>
     </div>
   );
 }
