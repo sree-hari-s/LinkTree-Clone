135f6c6fb7f807f28f9b0a701a89e139789c8f67
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-08-24 00:03:57 +0530

    component added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 
-import {Avatar} from "primereact/avatar";
-import {Badge} from "primereact/badge";
+import {Button} from "primereact/button";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
     <div className="p-m-4">
-      <div className="p-d-flex p-jc-center p-ai-center">
-        <Avatar image="/logo512.png" size="xlarge" shape="circle" className="p-overlay-badge">
-          <Badge value="4" severity="info"/>
-        </Avatar>
-      </div>
-      <div className="p-d-flex p-jc-center">
-        <p>Testing LinkFree</p>
-      </div>
+      <Profile />
+      
+        <div className="p-d-flex p-jc-center">
+          <div className="p-d-flex p-flex-column">
+            <Button className="youtube p-p-3">
+            <i className="pi pi-youtube p-px-2"></i>
+            <span className="p-px-3">Youtube</span>
+            </Button>
+            <Button className="twitter p-p-3">
+            <i className="pi pi-twitter p-px-2"></i>
+            <span className="p-px-3">Twitter</span>
+            </Button>
+          </div>
+        </div>
     </div>
   );
 }
