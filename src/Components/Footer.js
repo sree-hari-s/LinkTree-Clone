a6e2cb935b3038327aebf4d5cfeb9918cc8687e6
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-08-24 11:21:24 +0530

    footer created

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div className="p-d-flex p-jc-center">
+            <div className="p-d-flex p-jc-center p-ai-center">
+                <p>Contribute to <i className="pi pi-github" aria-hidden="true"> </i>
+                <a href="https://github.com/sree-hari-s/React-test">Github Repository</a></p>
+            </div>
+        </div>
+    )
+}
+
+export  default Footer;
