f0042297202ff3954e4389ee9d84185463104302
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-08-24 16:19:05 +0530

    link opens in new tab

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
         <div className="p-d-flex p-jc-center">
             <div className="p-d-flex p-jc-center p-ai-center">
                 <p>Contribute to <i className="pi pi-github" aria-hidden="true"> </i>
-                <a href="https://github.com/sree-hari-s/React-test">Github Repository</a></p>
+                <a href="https://github.com/sree-hari-s/React-test" target="_blank">Github Repository</a></p>
             </div>
         </div>
     )
